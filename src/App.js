8964d224875fe5d704ca61218e86274d3f2bad79
ShitalJori98 <shital.jori@clairvoyantsoft.com>
2021-07-16 10:47:07 +0530

    project structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+     {/*  <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <h1 className="text-primary">HIII Girlsss</h1>
     </div>
   );
 }
